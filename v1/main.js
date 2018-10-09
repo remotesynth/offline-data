728c794ef1303982baa3e5bf4fd8548678a87ac6
Brian Rinaldi <brian.rinaldi@gmail.com>
2018-10-09 09:11:41 -0400

    Initial commit

---
+++ v1/main.js
@@ -0,0 +1,221 @@
+const APIROOT = 'https://fortnite-public-api.theapinetwork.com/prod09/';
+const CONTAINER = document.getElementById('main-container');
+const TODAY = new Date();
+const TODAYSTR = TODAY.getMonth()+1 + '/' + TODAY.getDate() + '/' + TODAY.getFullYear();
+
+function getStoreUpcoming() {
+  const request = new XMLHttpRequest();
+
+  document.getElementById('statsNav').classList.remove('active');
+  document.getElementById('storeNav').classList.remove('active');
+  document.getElementById('upcomingNav').classList.add('active');
+
+  request.open('GET', APIROOT + '/upcoming/get', true);
+  request.setRequestHeader('Authorization', FORTNITE_APIKEY);
+
+  request.onload = function() {
+    if (request.status >= 200 && request.status < 400) {
+      var data = JSON.parse(request.responseText);
+      CONTAINER.innerHTML = '<h3>Upcoming Items on ' + TODAYSTR + '</h3>';
+      displayStoreData(data);
+    } else {
+      console.log(request.statusText);
+    }
+  };
+
+  request.onerror = function() {
+    console.log('something went wrong');
+  };
+
+  request.send();
+}
+
+function getStore() {
+  const request = new XMLHttpRequest();
+
+  document.getElementById('statsNav').classList.remove('active');
+  document.getElementById('storeNav').classList.add('active');
+  document.getElementById('upcomingNav').classList.remove('active');
+
+  request.open('GET', APIROOT + '/store/get', true);
+  request.setRequestHeader('Authorization', FORTNITE_APIKEY);
+
+  request.onload = function() {
+    if (request.status >= 200 && request.status < 400) {
+      const data = JSON.parse(request.responseText);
+      CONTAINER.innerHTML = '<h3>Store Items for ' + TODAYSTR + '</h3>';
+      displayStoreData(data);
+    } else {
+      console.log(request.statusText);
+    }
+  };
+
+  request.onerror = function() {
+    console.log('something went wrong');
+  };
+
+  request.send();
+}
+
+function getUser(username, platform) {
+  const request = new XMLHttpRequest();
+  request.open('GET', APIROOT + '/users/id?username=' + username, true);
+  request.setRequestHeader('Authorization', FORTNITE_APIKEY);
+
+  request.onload = function() {
+    if (request.status >= 200 && request.status < 400) {
+      const data = JSON.parse(request.responseText);
+      if (data.uid && data.platforms.includes(platform)) {
+        sessionStorage.setItem('userid', data.uid);
+        sessionStorage.setItem('username', username);
+        sessionStorage.setItem('platform', platform);
+        getUserStats(data.uid, platform);
+      } else if (data.uid) {
+        console.log('the user was found but not for the specified platform');
+      } else {
+        console.log('the user was not found');
+      }
+    } else {
+      console.log(request.statusText);
+    }
+  };
+
+  request.onerror = function() {
+    console.log('something went wrong');
+  };
+
+  request.send();
+}
+
+function getUserStats(userid, platform) {
+  const request = new XMLHttpRequest();
+  CONTAINER.innerHTML = '<p>Loading...</p>';
+  request.open(
+    'GET',
+    APIROOT +
+      '/users/public/br_stats?user_id=' +
+      userid +
+      '&platform=' +
+      platform,
+    true
+  );
+  request.setRequestHeader('Authorization', FORTNITE_APIKEY);
+
+  request.onload = function() {
+    if (request.status >= 200 && request.status < 400) {
+      const data = JSON.parse(request.responseText);
+      CONTAINER.innerHTML = '';
+      displayUserData(data);
+    } else {
+      console.log(request.statusText);
+    }
+  };
+
+  request.onerror = function() {
+    console.log('something went wrong');
+  };
+
+  request.send();
+}
+
+function displayMyStats() {
+  document.getElementById('statsNav').classList.add('active');
+  document.getElementById('storeNav').classList.remove('active');
+  document.getElementById('upcomingNav').classList.remove('active');
+  
+  if (sessionStorage.getItem('userid') && sessionStorage.getItem('platform')) {
+    getUserStats(
+      sessionStorage.getItem('userid'),
+      sessionStorage.getItem('platform')
+    );
+  } else if (
+    sessionStorage.getItem('username') &&
+    sessionStorage.getItem('platform')
+  ) {
+    getUser(
+      sessionStorage.getItem('username'),
+      sessionStorage.getItem('platform')
+    );
+  } else {
+    CONTAINER.innerHTML = '';
+    displayForm();
+  }
+}
+
+function displayForm() {
+  CONTAINER.innerHTML = `
+    <form>
+    <div class="form-group">
+      <label for="username">Username:</label>
+      <input type="text" name="username" id="usernameInput" class="form-control"><br>
+    </div>
+    <div class="form-group">
+      <label for="username">Platform:</label>
+      <select name="platform" id="platformSelect" class="form-control">
+        <option value="ps4">Playstation</option>
+        <option value="xbox">XBox</option>
+        <option value="nintendo">Switch</option>
+        <option value="pc">PC</option>
+      </select>
+    </div>
+    <button id="submitForm" class="btn btn-primary mb-2">Submit</button>
+    </form>
+  `;
+  document
+    .getElementById('submitForm')
+    .addEventListener('click', handleFormSubmit);
+}
+
+function displayUserData(data) {
+  CONTAINER.innerHTML += `
+    <div class="col">
+      <h2>${data.username}/${data.platform}</h2>
+      <h4>Totals (all time)</h4>
+      <ul>
+        <li>Matches Played: ${data.totals.matchesplayed}</li>
+        <li>Kills: ${data.totals.kills}</li>
+        <li>Wins: ${data.totals.wins}
+          <ul>
+            <li>Solo: ${data.stats.placetop1_solo}</li>
+            <li>Duo: ${data.stats.placetop1_duo}</li>
+            <li>Squad: ${data.stats.placetop1_squad}</li>
+          </ul>
+        </li>
+        <li>Win Rate: ${data.totals.winrate}</li>
+        <li>K/D: ${data.totals.kd}</li>
+      </ul>
+      <a href="#" onclick="clearStats()">clear</a>
+    </div>
+    `;
+}
+
+function clearStats() {
+  sessionStorage.clear();
+  displayForm();
+}
+
+function displayStoreData(data) {
+  data.items.forEach(item => {
+    CONTAINER.innerHTML += `
+      <div style="width:33%;float:left;">
+        <img src="${item.item.images.background}" height="200">
+        <h3>${item.name}</h3>
+        <p><img src="${data.vbucks}" width="20"> ${
+          item.cost
+        }</p>
+      </div>`;
+  });
+}
+
+function handleFormSubmit() {
+  const username = document.getElementById('usernameInput').value;
+  const platform = document.getElementById('platformSelect');
+  if (username) {
+    getUser(username, platform[platform.selectedIndex].value);
+  }
+}
+
+document.getElementById('statsNav').addEventListener('click', displayMyStats);
+document.getElementById('storeNav').addEventListener('click', getStore);
+document.getElementById('upcomingNav').addEventListener('click', getStoreUpcoming);
+displayMyStats();
