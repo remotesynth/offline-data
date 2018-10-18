3dab101a855fc1ff3d72004aa209fcc120aad27c
Brian Rinaldi <brian.rinaldi@gmail.com>
2018-10-18 11:59:29 -0400

    even more refactoring

--- v5/main.js
+++ v5/main.js
@@ -129,13 +129,16 @@ function loadStoreData() {
 }
 
 function getUser(username, platform) {
-  const request = new XMLHttpRequest();
-  request.open('GET', APIROOT + '/users/id?username=' + username, true);
-  request.setRequestHeader('Authorization', FORTNITE_APIKEY);
-
-  request.onload = function() {
-    if (request.status >= 200 && request.status < 400) {
-      var data = JSON.parse(request.responseText);
+  fetch(APIROOT + '/users/id?username=' + username, {
+    headers: {
+      'Authorization':FORTNITE_APIKEY
+    }
+  }).then((response) => {
+    if (response.status !== 200) {
+      console.log('Status Code: ' + response.status);
+      return;
+    }
+    response.json().then((data) => {
       if (data.uid && data.platforms.includes(platform)) {
         localStorage.setItem('userid', data.uid);
         localStorage.setItem('username', username);
@@ -146,45 +149,33 @@ function getUser(username, platform) {
       } else {
         console.log('the user was not found');
       }
-    } else {
-      console.log(request.statusText);
-    }
-  };
-
-  request.onerror = function() {
-    console.log('something went wrong');
-  };
-
-  request.send();
+    });
+  }).catch((error) => {
+    console.log(error);
+  });
 }
 
 function getUserStats(userid, platform) {
-  const request = new XMLHttpRequest();
+  const requestUrl = APIROOT + '/users/public/br_stats?user_id=' + userid + '&platform=' + platform;
   CONTAINER.innerHTML = '<p>Loading...</p>';
-  request.open(
-    'GET',
-    APIROOT + '/users/public/br_stats?user_id=' + userid + '&platform=' + platform,
-    true
-  );
-  request.setRequestHeader('Authorization', FORTNITE_APIKEY);
-
-  request.onload = function() {
-    if (request.status >= 200 && request.status < 400) {
-      const data = JSON.parse(request.responseText);
-      localStorage.setItem('userdata', request.responseText);
-      localStorage.setItem('userdata-lastupdated', TODAY.toDateString())
+  fetch(requestUrl, {
+    headers: {
+      'Authorization':FORTNITE_APIKEY
+    }
+  }).then((response) => {
+    if (response.status !== 200) {
+      console.log('Status Code: ' + response.status);
+      return;
+    }
+    response.json().then((data) => {
+      localStorage.setItem('userdata', JSON.stringify(data));
+      localStorage.setItem('userdata-lastupdated', TODAY.toDateString());
       CONTAINER.innerHTML = '';
       displayUserData(data);
-    } else {
-      console.log(request.statusText);
-    }
-  };
-
-  request.onerror = function() {
-    console.log('something went wrong');
-  };
-
-  request.send();
+    });
+  }).catch((error) => {
+    console.log(error);
+  });
 }
 
 function displayMyStats() {
